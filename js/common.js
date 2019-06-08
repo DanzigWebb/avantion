715b97224fb71c7f8bba865586fec13639013608
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-06-08 22:52:30 +0300

    full

---
+++ js/common.js
@@ -0,0 +1,15 @@
+let animTitle = document.querySelector('#anim-title');
+let worldArr = animTitle.innerHTML.split('');
+let newTitle = document.createElement('div');
+
+worldArr.forEach(world => {
+  let titleWorld = document.createElement('span');
+  titleWorld.style.animationDelay = `${Math.ceil(Math.random() * 1000)}ms`;
+  // titleWorld.style.opacity = `1`;
+  titleWorld.innerHTML = `${world}`;
+  newTitle.appendChild(titleWorld);
+});
+
+
+animTitle.innerHTML = newTitle.innerHTML
+// console.log(newTitle)
