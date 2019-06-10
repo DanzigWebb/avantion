4082f77bcc98cd15884e17909304dd0ae1ba3f9f
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-06-11 00:02:41 +0300

    c

--- js/common.js
+++ js/common.js
@@ -12,8 +12,100 @@ worldArr.forEach(world => {
 });
 animTitle.innerHTML = newTitle.innerHTML
 
-// let humburger = document.getElementById('hamburger');
-// humburger.addEventListener('click', () => {
-//   humburger.classList.toggle('is-active')
-// })
 sidebar.init()
+
+
+let slider = {
+  sliderArea: document.querySelector('#my-slider'),
+  sliderWrap: document.querySelector('.slider-wrap'),
+  leftBtn: document.querySelector('#left'),
+  rightBtn: document.querySelector('#right'),
+  index: 0,
+  init: function () {
+    let thisHelper = this;
+    for (let i = 0; i < this.sliderArea.children.length; i++) {
+      this.sliderArea.children[i].classList.add('slider-item');
+      this.sliderArea.children[0].classList.add('active');
+    };
+    let sliderItems = document.querySelectorAll('.slider-item');
+
+    sliderItems.forEach(function (item, i, arr) {
+      thisHelper.sliderArea.style.width = thisHelper.sliderWrap.offsetWidth * arr.length + 'px';
+      item.style.width = thisHelper.sliderWrap.offsetWidth + 'px';
+      step = item.style.width;
+    });
+    // события
+    this.rightBtn.addEventListener('click', () => {
+      this.right(this.index, sliderItems);
+      console.log(this.index)
+    })
+    this.leftBtn.addEventListener('click', () => {
+      this.left(this.index, sliderItems);
+      console.log(this.index)
+    })
+  },
+
+
+
+  // методы
+  right: function (index, arr) {
+    index++;
+    this.sliderArea.style.transform = `translateX(-${index * arr[index].offsetWidth}px)`;
+    this.index = index;
+    // this.sliderArea[this.index].classList.add('active')
+  },
+  left: function (index, arr) {
+    index--;
+    this.sliderArea.style.transform = `translateX(-${index * arr[index].offsetWidth}px)`;
+    this.index = index;
+    // this.sliderArea[this.index].classList.add('active')
+  }
+}
+
+slider.init()
+
+let slslsl = function () {
+  let slider = document.querySelector('#my-slider');
+  let sliderWrap = document.querySelector('.slider-wrap')
+  let left = document.querySelector('#left');
+  let right = document.querySelector('#right');
+
+  let index = 0;
+  let step = 0;
+
+  // разметка
+  for (let i = 0; i < slider.children.length; i++) {
+    slider.children[i].classList.add('slider-item');
+    slider.children[index].classList.add('active');
+  };
+  let sliderItems = document.querySelectorAll('.slider-item');
+
+  sliderItems.forEach(function (item, i, arr) {
+    slider.style.width = sliderWrap.offsetWidth * arr.length + 'px';
+    item.style.width = sliderWrap.offsetWidth + 'px';
+    step = item.style.width;
+  });
+
+
+
+  // события
+  left.addEventListener('click', () => {
+    sliderLeft();
+  })
+
+  right.addEventListener('click', () => {
+    sliderRight();
+  })
+
+  // методы
+  let sliderLeft = function () {
+    slider.style.transform = `translateX(-${index * step})`
+  }
+
+  let sliderRight = function () {
+    slider.style.transform = `translateX(${index * step})`
+  }
+}
+
+
+
