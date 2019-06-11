803d5d6262c22a47c2a67ed08e7537be00019197
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-06-11 23:13:13 +0300

    c

--- js/common.js
+++ js/common.js
@@ -1,16 +1,19 @@
-let animTitle = document.querySelector('#anim-title');
-let worldArr = animTitle.innerHTML.split('');
-let newTitle = document.createElement('div');
-let animTime = 0;
+let animTitle = function () {
+  let animTitle = document.querySelector('#anim-title');
+  let worldArr = animTitle.innerHTML.split('');
+  let newTitle = document.createElement('div');
 
-worldArr.forEach(world => {
-  let titleWorld = document.createElement('span');
+  worldArr.forEach(world => {
+    let titleWorld = document.createElement('span');
 
-  titleWorld.style.animationDelay = `${Math.ceil(Math.random() * 1200) + 50}ms`;
-  titleWorld.innerHTML = `${world}`;
-  newTitle.appendChild(titleWorld);
-});
-animTitle.innerHTML = newTitle.innerHTML
+    titleWorld.style.animationDelay = `${Math.ceil(Math.random() * 1200) + 50}ms`;
+    titleWorld.innerHTML = `${world}`;
+    newTitle.appendChild(titleWorld);
+  });
+  animTitle.innerHTML = newTitle.innerHTML
+}
+animTitle()
+
 
 sidebar.init()
 
@@ -36,76 +39,60 @@ let slider = {
     });
     // события
     this.rightBtn.addEventListener('click', () => {
-      this.right(this.index, sliderItems);
+      this.sliderRight();
       console.log(this.index)
     })
     this.leftBtn.addEventListener('click', () => {
-      this.left(this.index, sliderItems);
+      this.sliderLeft(this.index, sliderItems);
       console.log(this.index)
     })
+    // тач события
+    let startx = 0;
+    this.sliderWrap.addEventListener('touchstart', function (e) {
+      let touchobj = e.changedTouches[0] // первая точка прикосновения
+      startx = parseInt(touchobj.clientX) // положение точки касания по x, относительно левого края браузера
+    }, false)
+    this.sliderWrap.addEventListener('touchend', function (e) {
+      let touchobj = e.changedTouches[0] // первая точка прикосновения для данного события
+      let oYlength = touchobj.clientX - startx;
+      if (oYlength < -50) thisHelper.sliderRight();
+      else if (oYlength > 50) thisHelper.sliderLeft(thisHelper.index, sliderItems);
+    }, false)
   },
-
-
-
   // методы
-  right: function (index, arr) {
-    index++;
-    this.sliderArea.style.transform = `translateX(-${index * arr[index].offsetWidth}px)`;
-    this.index = index;
-    // this.sliderArea[this.index].classList.add('active')
+  sliderRight: function () {
+    let arr = this.sliderArea.children;
+    if (this.index + 1 == arr.length) {
+      this.index = 0;
+      this.sliderGo();
+    } else {
+      this.index++;
+      this.sliderGo();
+      this.index = this.index;
+    }
   },
-  left: function (index, arr) {
-    index--;
-    this.sliderArea.style.transform = `translateX(-${index * arr[index].offsetWidth}px)`;
-    this.index = index;
-    // this.sliderArea[this.index].classList.add('active')
-  }
+  sliderLeft: function () {
+    let arr = this.sliderArea.children;
+    if (this.index == 0) {
+      this.index = arr.length - 1;
+      this.sliderGo();
+    } else {
+      this.index--;
+      this.sliderGo();
+    }
+  },
+  sliderGo: function () {
+    let arr = this.sliderArea.children;
+    this.sliderArea.style.transform = `translateX(-${this.index * arr[this.index].offsetWidth}px)`;
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].classList.remove('active');
+      arr[this.index].classList.add('active')
+    }
+  },
+
 }
 
 slider.init()
 
-let slslsl = function () {
-  let slider = document.querySelector('#my-slider');
-  let sliderWrap = document.querySelector('.slider-wrap')
-  let left = document.querySelector('#left');
-  let right = document.querySelector('#right');
-
-  let index = 0;
-  let step = 0;
-
-  // разметка
-  for (let i = 0; i < slider.children.length; i++) {
-    slider.children[i].classList.add('slider-item');
-    slider.children[index].classList.add('active');
-  };
-  let sliderItems = document.querySelectorAll('.slider-item');
-
-  sliderItems.forEach(function (item, i, arr) {
-    slider.style.width = sliderWrap.offsetWidth * arr.length + 'px';
-    item.style.width = sliderWrap.offsetWidth + 'px';
-    step = item.style.width;
-  });
-
-
-
-  // события
-  left.addEventListener('click', () => {
-    sliderLeft();
-  })
-
-  right.addEventListener('click', () => {
-    sliderRight();
-  })
-
-  // методы
-  let sliderLeft = function () {
-    slider.style.transform = `translateX(-${index * step})`
-  }
-
-  let sliderRight = function () {
-    slider.style.transform = `translateX(${index * step})`
-  }
-}
-
-
-
+console.log(slider.sliderItems)
